a9b7b15b9018a128c120a7dbff0411fc5890de18
FrancescoSls97 <frankds1997@gmail.com>
2025-05-30 11:09:34 +0200

    js snack

---
+++ js-snack1/js/main.js
@@ -0,0 +1,9 @@
+const num1 = prompt("Inserisci un numero");
+const num2 = prompt("inserisci un numero");
+
+if (num1 > num2) {
+  console.log(num1);
+} else {
+  num2 > num1;
+  console.log(num2);
+}
