8868b822822e5a0081c30d50ede7d82124b70083
FrancescoSls97 <frankds1997@gmail.com>
2025-05-30 12:05:35 +0200

    js snacl 3

--- js-snack1/js/main.js
+++ js-snack1/js/main.js
@@ -3,7 +3,5 @@ const num2 = Number(prompt("inserisci un numero"));
 
 if (num1 > num2) {
   console.log(num1);
-} else if {
-  (num2 > num1)
-  console.log(num2);
-} else console.log("Numeri uguali")
+} else if (num2 > num1) console.log(num2);
+else console.log("Numeri uguali");
---
+++ js-snack3/js/main.js
@@ -0,0 +1,15 @@
+let Num1 = Number(prompt("Inserisci un numero"));
+let Num2 = Number(prompt("Inserisci un numero"));
+let Num3 = Number(prompt("Inserisci un numero"));
+let Num4 = Number(prompt("Inserisci un numero"));
+let Num5 = Number(prompt("Inserisci un numero"));
+let Num6 = Number(prompt("Inserisci un numero"));
+let Num7 = Number(prompt("Inserisci un numero"));
+let Num8 = Number(prompt("Inserisci un numero"));
+let Num9 = Number(prompt("Inserisci un numero"));
+let Num10 = Number(prompt("Inserisci un numero"));
+
+let somma =
+  Num1 + Num2 + Num3 + Num4 + Num5 + Num6 + Num7 + Num8 + Num9 + Num10;
+
+console.log(somma);
